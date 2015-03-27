d5e4c7474c5201465087b0a49c9ff875ef5bdaf6
Ahmed Samara <asamara@ncsu.edu>
2015-03-27 03:41:22 -0400

    Stubbed out parser

---
+++ ExpressionParser.java
@@ -0,0 +1,36 @@
+public class ExpressionParser
+{
+    Public ExpressionParser(String expression);
+
+
+
+    public static void main(String[] args)
+    {
+        //not run in final version
+    }
+
+    public ExpressionParser()
+    {
+    }
+
+    public static double parse_expression(String exp)
+    {
+        //API to outside world. Returns evaluated outside value.
+    }
+
+    public static String remove_spaces(String exp)
+    {
+        //Returns expression without spaces/
+        //Returns errors when 
+    }
+
+    public static String replace_consts(String exp)
+    {
+
+    }
+
+    public static String smallest_exp(String exp)
+    {
+    }
+
+}
